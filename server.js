70f05cf9d9dd90b756106d13cd793aaf193c5275
EdsonSizenando <edson.sizenando@gmail.com>
2025-10-20 22:53:24 -0300

    API / NODE.JS / BANCO DE DADOS
    Criando API com node JS e usando banco de dados. PARTE 1 concluida.

--- server.js
+++ server.js
@@ -7,8 +7,14 @@
 */
 
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
+app.use(express.json())  //fazer o express utilzar json
+
+
 
 //criando rota p/ listar ou mostrar informação
 /* 1 Tipo de rota/ metodo HTTP
@@ -16,12 +22,94 @@ const app = express()
 
 */
 //req -> requisição, res -> resposta
-app.get('/usuarios', (req, res) => {
-    res.send('Ok, Deu bom')
-})  
+//criar usuario sempre no metodo POST
+
+
+app.post('/usuarios', async (req, res) => {
+   
+      await prisma.user.create({
+         data: {
+            email: req.body.email,
+            name : req.body.name,
+            age: req.body.age
+         }
+      })
+      
+
+      res.status(201).json(req.body)   
+
+})
+
+
+
+app.get('/usuarios', async(req, res) => {
+    
+   let users = []
+   
+   if (req.query) {
+      users = await prisma.user.findMany({
+         where: {
+            name: req.query.name,
+            email: req.query.email,
+            age: req.query.age
+         }
+      })
+
+   } else {
+      users = await prisma.user.findMany()
+
+   }
+
+    
+
+
+   res.status(200).json(users)
+}) 
+
+
+app.put('/usuarios/:id', async (req, res) => {
+      
+     await prisma.user.update({
+         where: {
+            id: req.params.id
+         },
+         data: {
+            email: req.body.email,
+            name : req.body.name,
+            age: req.body.age
+         }
+      }) 
+      
+
+      res.status(201).json(req.body)   
+
+})
+
+
+app.delete('/usuarios/:id', async (req, res) => {
+   await prisma.user.delete({
+      where: {
+         id: req.params.id
+      },
+   })
+
+
+   res.status(200).json({message: "Usuário deletado com sucesso!"})
+})
 
 app.listen(3000) // porta onde o servidor vai rodar
+
+
+
 
 //node server.js - digitar no terminal para o servidor rodar
 
 // antes ir ao package.json e digitar   "type": "module", isso avisa sobre o metodo utilizado
+
+/*
+   objetivo =  Criar nossa API de usuários
+      - Criar um usuario
+      - Listar todos os usuarios
+      - editar usuarios
+      - deletar um usuario
+   */
